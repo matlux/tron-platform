6766c5ef3d90715a81d9d40148719a47981a45e0
nabacg <nabacg@gmail.com>
2014-07-17 17:30:02 +0100

    direction ordering

--- src/tron/bots.clj
+++ src/tron/bots.clj
@@ -28,14 +28,29 @@
 (defn turner
   "I'll just turn around"
   [look {[x y] :pos}]
-  (reset! look-fn look)
+  ;(reset! look-fn look)
   (let [dirs (get-dirs)
         move-options (map new-pos dirs (cycle [[x y]]))
         new-pos (first
                  (filter #(safe-pos? look %)
                          move-options))]
-    {:pos
-     new-pos}))
+    {:pos new-pos}))
+
+(defn order-dirs [dir]
+  (if (or (= dir :south) (= dir :north))
+    [:east :west :north :south]
+    [:north :south :east :west]))
+
+(defn smart-turner [look {[x y] :pos dir :dir :or {dir :east}}]
+  (let [dirs (order-dirs dir)
+        move-options (map (fn [d p] [d (new-pos d p)]) dirs (cycle [[x y]]))
+        [new-dir new-pos] (first
+                           (filter (fn [[d p]] (safe-pos? look p))
+                                   move-options))]
+    {:pos new-pos
+     :dir new-dir}))
+
+
 ;;next recursive walker, looks few steps ahead
 ;; makes one decision and then re-evaluates based on outcome of
 ;; move from this future step
@@ -88,7 +103,7 @@
     (tron/spawn-biker buzz red)
     (tron/spawn-biker south-walker green)
     (tron/spawn-biker turner orange)
-    (tron/spawn-biker farseer blue)
+    (tron/spawn-biker smart-turner blue)
     )
   )
 
