4018fdbbf7d7c52b44c6ce6ff2f6d3f6fdb46c39
robbie70 <robbie70@hotmail.com>
2014-08-20 13:01:21 +0100

    turning snake

--- src/tron/bots.clj
+++ src/tron/bots.clj
@@ -1,15 +1,46 @@
 (ns tron.bots
   (:require [tron.core :as tron]))
 
+(defn isSafe [look pos]
+  ;(println "is safe: " pos)
+  (not (look pos)))
+
+(defn go-west [[x y]]
+  [(dec x) y]
+  )
+
+(defn go-east [[x y]]
+  [(inc x) y]
+  )
+
+ (defn go-north [[x y]]
+  [x (dec y)]
+  )
+
+ (defn go-south [[x y]]
+  [x (inc y)]
+  )
+
+(defn rob-rider [look {pos :pos}]
+  ;(println "!!!!!!!!!!!!!!" pos)
+  (let [new-pos (cond 
+      (isSafe look (go-west pos)) (go-west pos)
+      (isSafe look (go-east pos)) (go-east pos)
+      (isSafe look (go-north pos)) (go-north pos)
+      (isSafe look (go-south pos)) (go-south pos)
+      :default [3 3]
+    )]
+  ;(println "new pos " new-pos)
+  {:pos new-pos}
+  ))
+
+
 ; Implement a strategy similar to Buzz!!
 (defn buzz
   "To infinity and beyond!"
   [look {[x y] :pos}]
   {:pos [(inc x) y]})
-(defn buzz2
-  "To infinity and beyond!"
-  [look {[x y] :pos}]
-  {:pos [(inc x) y]})
+
 
 ; Choose a TEAM colour
 (def red 1)
@@ -34,11 +65,17 @@
     (tron/stop!)
     (tron/blank-arena)
     (tron/spawn-biker buzz red)
-    (tron/spawn-biker buzz2 yellow)
+    ;(tron/spawn-biker buzz2 yellow)
+    ;(tron/spawn-biker rob purple)
+    (tron/spawn-biker rob-rider green)
     )
   )
 
 (defn -main []
   (start))
 
-
+(defn stop []
+  (do
+    (tron/stop!)
+    )
+  )
